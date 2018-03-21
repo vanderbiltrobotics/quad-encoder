ea2b814505e24793dca6fcdc3144a2974cd85376
Josh Petrin <joshua.m.petrin@vanderbilt.edu>
2018-03-21 00:18:41 +0000

    initial code for optical encoder

---
+++ encoder.cpp
@@ -0,0 +1,14 @@
+#include <math.h>
+#include <roboticscape.h>
+#include <unistd.h>
+#include <stdio.h>
+extern "C" {
+  #include <roboticscape.h>
+  #include <rc_usefulincludes.h>
+}
+
+int main(void)
+{
+  std::cout << "Hello!" << std::endl;
+  return 0;
+}
