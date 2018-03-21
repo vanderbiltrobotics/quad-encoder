ed9275a5dc420e3f740b76e3673650157649ffca
Josh Petrin <joshua.m.petrin@vanderbilt.edu>
2018-03-21 00:30:50 +0000

    Test code for reading the position at a single point (not able to ascert the actual amount of rotations made

--- encoder.cpp
+++ encoder.cpp
@@ -9,6 +9,16 @@ extern "C" {
 
 int main(void)
 {
-  std::cout << "Hello!" << std::endl;
-  return 0;
+  double ch1pos;
+
+  rc_initialize();
+
+  int runs = 200;
+
+  while(runs){
+    // position of enocder attached to port A
+    ch1pos = int rc_get_encoder_pos(1); 
+    printf("Current position: %f\n", ch1pos);
+  }
+  printf(" All readings completed.\n");
 }
