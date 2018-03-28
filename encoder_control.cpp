2edb34605cbd359a301c2b8ec2dcc4c353e6d79d
Josh Petrin <joshua.m.petrin@vanderbilt.edu>
2018-03-28 03:03:10 +0000

    Test code for encoder and motor control

--- encoder_control.cpp
+++ encoder_control.cpp
@@ -2,7 +2,7 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdint.h>
-#include <time.h>
+#include <ctime>
 
 extern "C" {
 	#include <roboticscape.h>
@@ -10,22 +10,32 @@ extern "C" {
 //	#include <rc_defs.h>
 }
 
-int main(void){
+int main(void) {
 
-	double ch1pos, ch1rev;
+	double ch2pos, ch2rev;
 
-	uint64_t ptime = 0;
+	//uint64_t ptime = 0;
+	struct timespec ptime, ctime;
 
-	rc_intialize();
-
-	while(true){ 
-		
+	rc_initialize();
+	rc_send_esc_pulse_normalized(1, 0.005);
+	int dispcount = 10000;
+	while(true) {
 		ch2pos = rc_get_encoder_pos(2);
 		ch2rev = ch2pos / 255;
-		if(time_t(NULL) >= ptime + 0.015){
+		if (dispcount <= 0) {
+			printf("Position: %f\n Revolution: %f\n", ch2pos, ch2rev);
+			dispcount = 10000;
+		} else {
+			--dispcount;
+		}
+		//rc_send_esc_pulse_normalized(1, 0.005);
+		clock_gettime(CLOCK_MONOTONIC, &ctime);
+		if (difftime(ctime.tv_sec, ptime.tv_sec)*1000.0 + (ctime.tv_nsec - ptime.tv_nsec)/(1000.0*1000.0) >= 15.0) {
 			rc_send_esc_pulse_normalized(1, 0.005);
-			ptime = time_t(NULL) + 0.02;
-		//	printf("Pulse generated at %f\n", ptime);
+			//ptime = time_t(NULL);
+			printf("Pulse generated\n");
+			clock_gettime(CLOCK_MONOTONIC, &ptime);
 		}
 	}
 }
