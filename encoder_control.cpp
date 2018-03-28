1018b2c93ef0709fad51cb98b1275c90d260a995
Josh Petrin <joshua.m.petrin@vanderbilt.edu>
2018-03-28 02:02:35 +0000

    Test file to test accuracy of the encoder

--- encoder_control.cpp
+++ encoder_control.cpp
@@ -1,19 +1,20 @@
 #include <math.h>
-#include <unitsd.h>
+#include <unistd.h>
 #include <stdio.h>
 #include <stdint.h>
 #include <time.h>
 
 extern "C" {
 	#include <roboticscape.h>
-	#include <rc_usefullincludes.h>
+	#include <rc_usefulincludes.h>
+//	#include <rc_defs.h>
 }
 
 int main(void){
 
 	double ch1pos, ch1rev;
 
-	unit64_t ptime = time_t(0);
+	uint64_t ptime = 0;
 
 	rc_intialize();
 
@@ -21,4 +22,10 @@ int main(void){
 		
 		ch2pos = rc_get_encoder_pos(2);
 		ch2rev = ch2pos / 255;
-		if(time_t
+		if(time_t(NULL) >= ptime + 0.015){
+			rc_send_esc_pulse_normalized(1, 0.005);
+			ptime = time_t(NULL) + 0.02;
+		//	printf("Pulse generated at %f\n", ptime);
+		}
+	}
+}
